ae8e1a9977800d4c1bde48cc3f35761ce3265ca6
betulgumus <153992737+betulgumus@users.noreply.github.com>
2025-12-20 23:41:10 +0300

    son03

--- webproje1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace webproje1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
